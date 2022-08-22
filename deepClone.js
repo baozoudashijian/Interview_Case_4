72bd43d4953444e705415c0329dc0d44fd1c0bf6
Roger Zhang <1787009521@qq.com>
2022-08-22 20:42:01 +0800

    feat: 完成深克隆

---
+++ deepClone.js
@@ -0,0 +1,28 @@
+
+const deepClone = (a) => {
+    if(a instanceof Object) {
+        let result = undefined
+        if(a instanceof Function) {
+            if(a.prototype) {
+                result = function() { return a.apply(this, arguments) }
+            }else{
+                result = (...args) => { return a.call(undefined, ...args) }
+            }
+        }else if( a instanceof Array) {
+            result = []
+        }else if(a instanceof Date) {
+            result = new Date(a-0)
+        }else if(a instanceof RegExp) {
+            result = new RegExp(a.source, a.flags)
+        }else{
+            result = {}
+        }
+
+        for(let k in a) {
+            result[k] = deepClone(a[k])
+        }
+        return result
+    }else{
+        return a
+    }
+}
