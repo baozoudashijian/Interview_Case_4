ceaf55450b81c042c0ebd7dbe80d0c41027c79b1
Roger Zhang <1787009521@qq.com>
2022-08-23 21:16:26 +0800

    feat: 选择排序

--- deepClone.js
+++ deepClone.js
@@ -1,28 +1,28 @@
 
 const deepClone = (a) => {
-    if(a instanceof Object) {
+    if (a instanceof Object) {
         let result = undefined
-        if(a instanceof Function) {
-            if(a.prototype) {
-                result = function() { return a.apply(this, arguments) }
-            }else{
+        if (a instanceof Function) {
+            if (a.prototype) {
+                result = function () { return a.apply(this, arguments) }
+            } else {
                 result = (...args) => { return a.call(undefined, ...args) }
             }
-        }else if( a instanceof Array) {
+        } else if (a instanceof Array) {
             result = []
-        }else if(a instanceof Date) {
-            result = new Date(a-0)
-        }else if(a instanceof RegExp) {
+        } else if (a instanceof Date) {
+            result = new Date(a - 0)
+        } else if (a instanceof RegExp) {
             result = new RegExp(a.source, a.flags)
-        }else{
+        } else {
             result = {}
         }
 
-        for(let k in a) {
+        for (let k in a) {
             result[k] = deepClone(a[k])
         }
         return result
-    }else{
+    } else {
         return a
     }
 }
